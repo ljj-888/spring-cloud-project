6866c7daa285c378f33c0f21e8c5431d33b29bf9
hello <975409698@qq.com>
2023-04-04 08:28:17 +0800

    启动微服务eureka客户端，并且完成了客户端和服务端的显示

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -3,11 +3,13 @@ package cn.itcast.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
+//@EnableEurekaClient
 public class OrderApplication {
 
     public static void main(String[] args) {
